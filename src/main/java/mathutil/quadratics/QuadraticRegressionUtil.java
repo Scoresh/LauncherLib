091539b167afa47d8d86ca7b5984ffeeb1b1ac02
daniel_sab <danielsabalakov@gmail.com>
2026-02-19 15:29:35 -0500

    quadreg template

---
+++ src/main/java/mathutil/quadratics/QuadraticRegressionUtil.java
@@ -0,0 +1,7 @@
+package mathutil.quadratics;
+
+// IF I EVER NEED TO CALCLAUTE QUADRATIC REGRESSION I'LL WRITE THIS
+// https://www.emathhelp.net/calculators/probability-statistics/quadratic-regression-calculator/
+public class QuadraticRegressionUtil {
+    
+}
