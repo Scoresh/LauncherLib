1a9b97d31068947cdd86e1a126ac9f66fb43f8e8
daniel_sab <danielsabalakov@gmail.com>
2026-02-19 15:30:58 -0500

    quadreg template 2

--- src/main/java/mathutil/quadratics/QuadraticRegressionUtil.java
+++ src/main/java/mathutil/quadratics/QuadraticRegressionUtil.java
@@ -1,7 +1,14 @@
 package mathutil.quadratics;
 
+import java.util.NoSuchElementException;
+
+import functions.QuadraticFunction;
+import mathutil.Point;
+
 // IF I EVER NEED TO CALCLAUTE QUADRATIC REGRESSION I'LL WRITE THIS
 // https://www.emathhelp.net/calculators/probability-statistics/quadratic-regression-calculator/
 public class QuadraticRegressionUtil {
-    
+    public QuadraticFunction CalculateQuadraticRegression(Point... points) {
+        throw(new NoSuchElementException("This has not been implemented yet. Crashing program."));
+    }
 }
